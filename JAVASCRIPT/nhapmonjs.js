281fb55887422911dd80643a5bef3a32689babc9
DTV <dthang277@gmail.com>
2022-05-08 21:58:02 +0700

    thêm bài tập

---
+++ JAVASCRIPT/nhapmonjs.js
@@ -0,0 +1,13 @@
+// function showMessager(){
+//     alert('Xin chào');
+ }
+function demo1(){
+    document.getElementByid("demo31").innerHTML=" Xin chào";
+ }
+// // function demo2(){
+// //     document.write("Hello!");}
+// function demo3() {
+//     console.log( Hi!);}
+// function showMessage(){
+//     document.getElementById('mm').innerHTML = 'Xin chào!';
+// }
