104e90ee9ae4df5264d7ede9104ce9a5b210c9a5
DTV <dthang277@gmail.com>
2022-05-18 20:39:30 +0700

    day len

--- HTML/HTML_Lesson13_Mang2/Luyentap1/bai7.js
+++ HTML/HTML_Lesson13_Mang2/Luyentap1/bai7.js
@@ -2,14 +2,23 @@ function bai7(){
     creatArray()
     let number = +prompt("Nhập số nguyên a")
     let count = 0
+    let iValue
     for (let i = 0; i < array.length; i++) {
         if (array[i] == number) {
             count+=1
+            iValue = i
         }
     }
     if (count != 0) {
-        array.splice(i,1)
+        array.splice(iValue,1)
         array.push("0")
         document.write(array + " Nằm trong dãy")
     } else
         document.write("Không nằm trong dãy")}
+//------//
+// if (count !=0){
+// for (iValue=0;iValue< array.length;iValue++){
+//     array.splice(iValue,1)
+//     array.push("0")
+// }document.write(array + " Nằm trong dãy")}else
+// { document.write("Khong nam trong")}
